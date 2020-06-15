333c91271b2d28a04f018f6628910f0c2482cedd
Ian Davids <idavids@roblox.com>
2020-06-15 12:02:51 -0700

    This is crap! Need generic dependency check and install.

--- tableeditor.py
+++ tableeditor.py
@@ -3,14 +3,21 @@ import sublime
 
 PCKGCTRL_SETTINGS = "Package Control.sublime-settings"
 TABLE_PACKAGE     = "Table Editor"
+# TODO Make this much more generic!
+#      support the languages and grammars we support.
+PS_PACKAGE        = "Powershell"
 
 def IsInstalled():
 	settings = sublime.load_settings(PCKGCTRL_SETTINGS)
 	return TABLE_PACKAGE in set(settings.get("installed_packages", []))
 
+def IsPkgInstalled(pkgName):
+	settings = sublime.load_settings(PCKGCTRL_SETTINGS)
+	return pkgName in set(settings.get("installed_packages", []))
+
 def Install():
 	print("Installing `{}` ..".format(TABLE_PACKAGE))
-	sublime.active_window().run_command("advanced_install_package", {"packages": TABLE_PACKAGE})
+	sublime.active_window().run_command("advanced_install_package", {"packages": TABLE_PACKAGE+","+PS_PACKAGE})
 	#Hide()
 
 def Hide():
