4d98d9cf2e9a7b7cd13caa11a9de8b1122865e1f
Ian Davids <ihdavids@gmail.com>
2021-01-16 01:19:52 -0800

    Thinking about what to do about orgmouse handler

--- orgmouse.py
+++ orgmouse.py
@@ -4,7 +4,6 @@ import OrgExtended.pymitter as evt
 
 class OrgMouseHandlerCommand(sublime_plugin.TextCommand):
     def run(self, edit, event=None):
-        print("HI")
         pt = 0 if not event else self.view.window_to_text((event["x"], event["y"]))
         evt.Get().emit("orgmouse", pt, self.view, edit)
 
