6c3e9e51514b0c39fee766bbf1cecdb76a798b7f
Ian Davids <ihdavids@gmail.com>
2021-02-10 10:23:05 -0800

    Added rudimentary comment blocks to generated syntax

--- orginternalhelpers.py
+++ orginternalhelpers.py
@@ -31,10 +31,235 @@ template = """
         1: constant.other orgmode.fence.sourceblock"""
 
 
+introBlock = """
+    // GENERATED: By OrgExtended
+    //
+    // The generator adds a subset of the orgmode specific scopes.
+    // The scopes that it has added tend to play an important role
+    // in making an orgmode buffer operational.
+    //
+    // That said, orgmode offers a wide variety of syntax elements for
+    // you to style as needed. Please see blow for more information
+    // on some of these scopes.
+"""
+
 # TODO Create blocks for each of the relevant blocks of markers
 #      Create a set of useful markers with comments about how you can customize them
 commentBlock = """
-    // This is a comment block and should be ignored
+	// GENERATED: By Org Extended
+    // 
+    // The generator has added a bunch of useful extensions to the color scheme
+    // That said there is much more than can be done by you to tweak your scheme
+    // to your hearts content. The following comment block is here to give you
+    // ideas of what is possible.
+    //
+    // On a windows box type Ctrl + Alt + Shift + P
+    // to get a view of what scopes are in play on the thing you want to style
+    // This table can help you tweak what you would like to see change
+    //
+	//	{
+	//		"scope": "orgmode.break",
+	//		"foreground": "#ed188a",
+	//	},
+	//	{
+	//		"scope": "orgmode.page",
+	//		"foreground": "#f5eebf",
+	//	},
+	//	{
+	//		"scope": "orgmode.headline",
+	//		"foreground": "#14adfa",
+	//		"background": "#172822",
+	//		"font_style": "bold italic",
+	//	},
+	//	{
+	//		"scope": "orgmode.headline2",
+	//		"background": "#172822",			
+	//		"foreground": "#bb86fc",
+	//		"font_style": "bold underline",
+	//	},
+	//	{
+	//		"scope": "orgmode.headline3",
+	//		"foreground": "#03dac5",
+	//		"font_style": "bold",
+	//	},
+	//	{
+	//		"scope": "orgmode.headline4",
+	//		"foreground": "#dfe6a1",
+	//		"font_style": "italic",
+	//	},
+	//	{
+	//		"scope": "orgmode.headline5",
+	//		"foreground": "#018786",
+	//		"font_style": "italic",
+	//	},
+	//	{
+	//		"scope": "orgmode.headline6",
+	//		"foreground": "#afe3de",
+	//		"font_style": "italic",
+	//	},
+	//	// Links can target these
+	//	{
+	//		"scope": "orgmode.target",
+	//		"foreground": "#7c004a",
+	//		"font_style": "italic",
+	//	},
+	//	// Links can target these
+	//	{
+	//		"scope": "orgmode.target.bracket",
+	//		"foreground": "#7c004a",
+	//		"font_style": "bold",
+	//	},
+	//	// [[LINK]] This is the entire link block
+	//	{
+	//		"scope": "orgmode.link",
+	//		"foreground": "#3cd7fa",
+	//		"font_style": "",
+	//	},
+	//	{
+	//		"scope": "orgmode.link.href",
+	//		"foreground": "#9999aa",
+	//		"font_style": "italic",
+	//	},
+	//	{
+	//		"scope": "orgmode.link.text",
+	//		"foreground": "#4ce7fd",
+	//		"font_style": "bold",
+	//	},
+	//  // Special coloring for email addresses
+	//	{
+	//		"scope": "orgmode.email",
+	//		"foreground": "#a188b3",
+	//		"font_style": "italic",
+	//	},
+	//	{
+	//		"scope": "orgmode.deadline",
+	//		"foreground": "#d1771d",
+	//		"font_style": "italic",
+	//	},
+	//	{
+	//		"scope": "orgmode.scheduled",
+	//		"foreground": "#d1771d",
+	//		"font_style": "italic",
+	//	},
+	//	// #+PRIORITIES: A B C
+	//	{
+	//		"scope": "orgmode.controltag",
+	//		"foreground": "#aaaaaa",
+	//	},
+	//	// controltag.text also exists
+	//	{
+	//		"scope": "orgmode.controltag.tag",
+	//		"foreground": "#aaaaaa",
+	//		"font_style": "italic",
+	//	},
+	//	// < DATETIME >
+	//	{
+	//		"scope": "orgmode.datetime",
+	//		"foreground": "#b0a497",
+	//	},
+	//	// [ DATETIME ]
+	//	{
+	//		"scope": "orgmode.unscheddatetime",
+	//		"foreground": "#b0a497",
+	//	},
+	//	// CLOSED: SCHEDULED: DEADLINE:		
+	//	{
+	//		"scope": "orgmode.statekeyword",
+	//		"foreground": "#d1771d",
+	//		"font_style": "italic",
+	//	},
+	//	{
+	//		"scope": "orgmode.checkbox",
+	//		"foreground": "#c9be7b",
+	//	},
+	//	{
+	//		"scope": "orgmode.checkbox.checked",
+	//		"foreground": "#00FF00",
+	//	},
+	//	{
+	//		"scope": "orgmode.checkbox.blocked",
+	//		"foreground": "#FF0000",
+	//	},
+	//	{
+	//		"scope": "orgmode.tags",
+	//		"foreground": "#ded49b",
+	//	},
+	//	{
+	//		"scope": "orgmode.tags.headline",
+	//		"foreground": "#deff9b",
+	//	},		
+	//	{
+	//		"scope": "orgmode.tack",
+	//		"foreground": "#c993c4",
+	//		"font_style": "bold",
+	//	},
+	//	{
+	//		"scope": "orgmode.numberedlist",
+	//		"foreground": "#c993c4",
+	//		"font_style": "bold",
+	//	},
+	//	{
+	//		"scope": "orgmode.definition",
+	//		"foreground": "#A2E8E4",
+	//		"font_style": "bold",
+	//	},
+	//	{
+	//		"scope": "orgmode.definition.marker",
+	//		"foreground": "#E1A2E8",
+	//		"font_style": "bold",
+	//	},
+	//	{
+	//		"scope": "orgmode.follow_up",
+	//		"foreground": "#FF0000",
+	//		"font_style": "bold",
+	//	},
+	//	{
+	//		"scope": "orgmode.fence",
+	//		"background": "#322830",
+	//		"font_style": "bold",
+	//	},
+	//	{
+	//		"scope": "orgmode.fence.language",
+	//		"background": "#322830",
+	//		"foreground": "#f1bff2",
+	//		"font_style": "bold",
+	//	},
+	//	{
+	//		"scope": "orgmode.raw.block",
+	//		"background": "#252520",
+	//		"font_style": "bold",
+	//	},
+	//	{
+	//		"scope": "orgmode.table.block",
+	//		"background": "#272828",
+	//	},
+	//	{
+	//		"scope": "orgmode.bold",
+	//		"foreground": "#aaffaa",
+	//		"font_style": "bold",
+	//	},
+	//	{
+	//		"scope": "orgmode.italics",
+	//		"foreground": "#aaffff",
+	//		"font_style": "italic",
+	//	},
+	//	{
+	//		"scope": "orgmode.underline",
+	//		"foreground": "#aaaaff",
+	//		"font_style": "underline",
+	//	},
+	//	{
+	//		"scope": "orgmode.strikethrough",
+	//		"foreground": "#aaaaaa",
+	//	},
+	//	{
+	//		"scope": "orgmode.code",
+	//		"foreground": "#ffaaff",
+	//	},
+	//	{
+	//		"scope": "orgmode.verbatim",
+	//		"foreground": "#ffaaaa",
+	//	},
 """
 
 class OrgRegenSyntaxTemplateCommand(sublime_plugin.TextCommand):
@@ -69,7 +294,7 @@ def findscope(cs, name):
 	if(name == None):
 		return None
 	for i in cs['rules']:
-		if i['scope'] == name:
+		if 'scope' in i and i['scope'] == name:
 			return i
 	return None
 
@@ -146,23 +371,33 @@ class OrgCreateColorSchemeFromActiveCommand(sublime_plugin.TextCommand):
 		self.origColorScheme = self.settings.get("color_scheme",None)
 		if(self.origColorScheme):
 			self.colorSchemeData = sublime.load_resource(self.origColorScheme)
+
 			cs = ast.literal_eval(self.colorSchemeData)
+			if(not cs):
+				print("FAILED TO GENERATE NEW COLOR SCHEME COULD NOT PARSE SCHEME")
+				return
+			path = os.path.join(sublime.packages_path(),"User","OrgColorSchemes")
+			if(not os.path.exists(path)):
+				os.mkdir(path)
+
+			cs['rules'].append({"COMMENT ORGMODE INTRO HERE":""})
+
 			self.addpreamble(cs)
 			self.addstates(cs)
 			self.addfences(cs)
 			self.addpriorities(cs)
 
-			path = os.path.join(sublime.packages_path(),"User","OrgColorSchemes")
-			if(not os.path.exists(path)):
-				os.mkdir(path)
 
 			scheme = os.path.basename(self.origColorScheme)
 			scheme = os.path.splitext(scheme)[0]
 			schemeName = scheme + "_Org.sublime-color-scheme"
 			outputFile = os.path.join(path, schemeName)
-			cs['rules'].append({"THIS SHOULD BE REPLACED":""})
+			cs['rules'].append({"COMMENT ORGMODE SCOPES HERE":""})
 			jsonStr = json.dumps(cs, sort_keys=True, indent=4)
-			jsonStr = jsonStr.replace('"THIS SHOULD BE REPLACED": ""',commentBlock)
+
+
+			jsonStr = jsonStr.replace('"COMMENT ORGMODE SCOPES HERE": ""',commentBlock)
+			jsonStr = jsonStr.replace('"COMMENT ORGMODE INTRO HERE": ""',introBlock)
 			with open(outputFile,'w') as ofile:
 				ofile.write(jsonStr)
 			newColorScheme = "Packages/User/OrgColorSchemes/" + schemeName
