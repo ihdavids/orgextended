2c1abb5ccc426a8ce37d9bfc1d66fbef65ce0d5e
Ian Davids <ihdavids@gmail.com>
2021-02-21 13:25:22 -0800

    Some bare bones table commands

--- orgtableformula.py
+++ orgtableformula.py
@@ -17,14 +17,38 @@ import OrgExtended.orginsertselected as ins
 
 log = logging.getLogger(__name__)
 
-def insert_file_data(data, view, edit, onDone=None):
+def insert_file_data(indentDepth, data, view, edit, onDone=None, replace=False):
     # figure out what our separator is.
     # replace the separator with |
+    indent = (" " * indentDepth) + " "
     lines = data.split('\n')
     data = ""
     for l in lines:
-        data += '|' + l.strip().replace(',','|') + '|\n'
-    view.run_command("org_internal_insert", {"location": view.sel()[0].begin(), "text": data, "onDone": onDone})
+        data += indent + '|' + l.strip().replace(',','|') + '|\n'
+    if(replace):
+        view.run_command("org_internal_replace", {"start": view.sel()[0].begin(), "end": view.sel()[0].end(), "text": data, "onDone": onDone})
+    else:
+        view.run_command("org_internal_insert", {"location": view.sel()[0].begin(), "text": data, "onDone": onDone})
+
+
+class OrgConvertSelectionToTableCommand(sublime_plugin.TextCommand):
+    def OnDone(self):
+        self.view.sel().clear()
+        self.view.sel().add(sublime.Region(self.pos.begin() +1, self.pos.begin()+1))
+        self.view.run_command('table_editor_next_field')
+        evt.EmitIf(self.onDone)
+
+    def run(self, edit, onDone=None):
+        self.edit = edit
+        self.onDone = onDone
+        self.pos = self.view.sel()[0]
+        curNode = db.Get().AtInView(self.view)
+        level = 1
+        if(None != curNode):
+            level = curNode.level
+        fileData = self.view.substr(self.view.sel()[0])
+        if(len(fileData) > 6):
+            insert_file_data(level,fileData,self.view, self.edit, evt.Make(self.OnDone), True)
 
 
 class OrgImportTableFromCsvCommand(sublime_plugin.TextCommand):
@@ -37,7 +61,11 @@ class OrgImportTableFromCsvCommand(sublime_plugin.TextCommand):
             with open(filename,'r') as f:
                 fileData = f.read()
             self.pos = self.view.sel()[0]
-            insert_file_data(fileData,self.view, self.edit, evt.Make(self.OnDone))
+            curNode = db.Get().AtInView(self.view)
+            level = 1
+            if(None != curNode):
+                level = curNode.level
+            insert_file_data(level,fileData,self.view, self.edit, evt.Make(self.OnDone))
 
     def OnDone(self):
         self.view.sel().clear()
@@ -54,15 +82,22 @@ class OrgImportTableFromCsvCommand(sublime_plugin.TextCommand):
 
 class OrgInsertBlankTableCommand(sublime_plugin.TextCommand):
     def OnDims(self, text):
+        if(text == None):
+            return
         dims = text.split('x')
         if(len(dims) != 2):
             log.error("DIMENSIONS ARE NOT RIGHT!")
             return
         w = int(dims[0])
         h = int(dims[1])
+        curNode = db.Get().AtInView(self.view)
+        level = 1
+        if(None != curNode):
+            level = curNode.level
+        indent = (" " * level) + " "
         data = ""
         for y in range(0,h+1):
-            line = "|"
+            line = indent + "|"
             for x in range(0,w):
                 line += "|"
             data += line + '\n'
