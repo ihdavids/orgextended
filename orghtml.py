d15ebaec4bca1796c1463e48a164b54ec1e6d5ef
Ian Davids <ihdavids@gmail.com>
2021-04-15 18:28:10 -0700

    Cleanup now that we have split the framework out.

--- orghtml.py
+++ orghtml.py
@@ -591,43 +591,6 @@ class HtmlExportHelper(exp.OrgExportHelper):
 
 # Export the entire file using our internal exporter
 class OrgExportFileOrgHtmlCommand(sublime_plugin.TextCommand):
-  def build_head(self, doc):
-    highlight      = exp.GetGlobalOption(self.file,"HTML_HIGHLIGHT","HtmlHighlight","zenburn").lower()
-    doc.AddInlineStyle(GetHighlightJsCss(highlight))
-    doc.AddInlineStyle(GetCollapsibleCss())
-    doc.AddInlineStyle(GetStyleData(self.style, self.file))
-    doc.AddExportMeta()
-
-  def build_node(self, doc, n):
-    doc.StartNode(n)
-    doc.NodeHeading(n)
-    doc.StartNodeBody(n)
-    doc.NodeBody(n)
-    for c in n.children:
-      self.build_node(doc, c)
-    doc.EndNodeBody(n)
-    doc.EndNode(n)
-
-  def build_nodes(self, doc):
-    if(self.index == None):
-      nodes = self.file.org
-      for n in nodes.children:
-        self.build_node(doc, n)
-    else:
-      n = self.file.org.env._nodes[self.index]
-      self.build_node(doc,n)
-
-  def build_document(self, doc):
-    doc.StartNodes()
-    self.build_nodes(doc)
-    doc.EndNodes()
-
-  def build_body(self, doc):
-    doc.StartDocument(self.file)
-    self.build_document(doc)
-    doc.EndDocument()
-    doc.InsertScripts(self.file)
-
   def OnDoneSourceBlockExecution(self):
     # Reload if necessary
     self.file = db.Get().FindInfo(self.view)
@@ -640,13 +603,6 @@ class OrgExportFileOrgHtmlCommand(sublime_plugin.TextCommand):
       doc.style      = self.style
       self.helper    = HtmlExportHelper(self.view, self.index)
       self.helper.Run(outputFilename, doc)
-      #doc.StartHead()
-      #self.build_head(doc)
-      #doc.EndHead()
-
-      #doc.StartBody()
-      #self.build_body(doc)
-      #doc.EndBody()
     finally:  
       evt.EmitIf(self.onDone)
 
@@ -667,9 +623,6 @@ class OrgExportFileOrgHtmlCommand(sublime_plugin.TextCommand):
       self.view.run_command('org_execute_all_source_blocks',{"onDone":evt.Make(self.OnDoneSourceBlockExecution),"amExporting": True})
     else:
       self.OnDoneSourceBlockExecution()
-
-  def clear_status(self):
-    self.view.set_status("ORG_EXPORT","")
 
 def sync_up_on_closed():
   notice.Get().BuildToday()
