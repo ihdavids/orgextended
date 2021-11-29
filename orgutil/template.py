d883239b4b5b65e0b2619bff2c5c2422a4f4e009
Ian Davids <ihdavids@gmail.com>
2021-11-28 16:09:20 -0800

    Small enhancements to template formatter.

--- orgutil/template.py
+++ orgutil/template.py
@@ -9,14 +9,37 @@ cur1 = re.compile('\\$0')
 
 # A really quick and dirty template mechanism.
 # Stolen from: https://makina-corpus.com/blog/metier/2016/the-worlds-simplest-python-template-engine
+#              https://github.com/ebrehault/superformatter
 class TemplateFormatter(string.Formatter):
     def __init__(self, resolver=None):
         super(TemplateFormatter, self).__init__()
         self.resolver = resolver
-    # {stringVal.upper:call} - make a function call to upper method 
     def format_field(self, value, spec):
-        if spec == 'call':
+        # REPITITION
+        #>>> sf.format('''Table of contents:
+        #{chapters:repeat:Chapter {{item}}
+        #}''', chapters=["I", "II", "III", "IV"])
+        #'''Table of contents:
+        #Chapter I
+        #Chapter II
+        #Chapter III
+        #Chapter IV
+        #'''
+        if spec.startswith('repeat'):
+            template = spec.partition(':')[-1]
+            if type(value) is dict:
+                value = value.items()
+            return ''.join([template.format(item=item) for item in value])
+        # FUNCTION CALLS
+        #>>> sf.format('My name is {name.upper:call}', name="eric")
+        #'My name is ERIC'
+        elif spec == 'call':
             return value()
+        # OPTIONAL EXPANSION
+        #>>> sf.format('Action: Back / Logout {manager:if:/ Delete {id}}', manager=True, id=34)
+        #'Action: Back / Logout / Delete 34'        
+        elif spec.startswith('if'):
+            return (value and spec.partition(':')[-1]) or ''
         else:
             return super(TemplateFormatter, self).format_field(value, spec)
     def get_value(self, key, args, kwargs):
@@ -28,7 +51,6 @@ class TemplateFormatter(string.Formatter):
             return None
         else:
             return args[key]
-
 
 def ExpandTemplate(view, template, format={},resolver=None):
     # Supported expansions
