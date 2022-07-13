a4f31ba562787be9252d6f57fcdc579a43cbbffa
Ian Davids <ihdavids@gmail.com>
2022-07-12 21:36:12 -0700

    Working on C# Babel support

--- orgsrc/csharp.py
+++ orgsrc/csharp.py
@@ -12,7 +12,7 @@ import OrgExtended.orgparse.date as odate
 import OrgExtended.orgutil.util as util
 import OrgExtended.orglist as lst
 
-# Javascript Babel Mode
+# C# Babel Mode
 
 def FormatText(txt):
     if(isinstance(txt,str)):
